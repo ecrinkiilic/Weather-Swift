2dfd71bfb4dec375600413d4982b43f30d3df213
Ecrin <ecrinkiilic07@gmail.com>
2026-07-03 02:29:49 +0200

    Add files via upload

---
+++ Hava durumu/ApiKey.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  WeatherManager
+//
+//
+
+import Foundation
+
+
+let apiKey = "0ba0f5f0491dfb9699ff3e76a9028a60"
+
+
---
+++ Hava durumu/Hava durumu/Model/Apikey.swift
@@ -0,0 +1,12 @@
+//
+//  Apikey.swift
+//  Hava durumu
+//
+//
+
+import Foundation
+
+let apiKey = "0ba0f5f0491dfb9699ff3e76a9028a60"
+
+
+
